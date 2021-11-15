ccdd709a1caffdf17e7f534494418c55a8b8d4f2
Luiz Timbó <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-11-15 16:06:07 -0300

    Interface de autenticacao

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "styled-components";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./src/routes/app.routes";
+import { SignIn } from "./src/scenes/SignIn";
 
 import {
   useFonts,
@@ -30,7 +31,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
---
+++ src/scenes/SignIn/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import GoogleSvg from "../../assets/google.svg";
+import AppleSvg from "../../assets/apple.svg";
+import LogoSvg from "../../assets/logo.svg";
+
+import { RFValue } from "react-native-responsive-fontsize";
+import {
+  Container,
+  Header,
+  TitleWrapper,
+  Title,
+  SignInTitle,
+  Footer,
+} from "./styles";
+
+export function SignIn() {
+  return (
+    <Container>
+      <Header>
+        <TitleWrapper>
+          <LogoSvg width={RFValue(120)} height={RFValue(68)} />
+        </TitleWrapper>
+
+        <Title>
+          Controle suas {"\n"} finanças de forma {"\n"} muito simples
+        </Title>
+        <SignInTitle>Faça seu login com {"\n"} uma das contas abaixo</SignInTitle>
+      </Header>
+
+      <Footer></Footer>
+    </Container>
+  );
+}

f636eab1f7d531ad3429769c5201949ce04c172d
Luiz Timbó <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-11-15 13:41:17 -0300

    Listando categorias

--- src/scenes/Resume/index.tsx
+++ src/scenes/Resume/index.tsx
@@ -1,17 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { HistoryCard } from "../../components/HistoryCard";
 
-import { Container, Header, Title } from "./styles";
+import { categories } from "../../utils/categories";
+import { Container, Header, Content, Title } from "./styles";
+
+interface TransactionData {
+  type: "positive" | "negative";
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
+
+interface CategoryData {
+  key: string;
+  name: string;
+  total: string;
+  color: string;
+}
 
 export function Resume() {
+  const dataKey = "@gofinances:transactions";
+
+  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+  async function loadData() {
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    const expensives = responseFormatted.filter(
+      (expensive: TransactionData) => expensive.type === "negative"
+    );
+
+    const totalByCategory: CategoryData[] = [];
+
+    categories.forEach((category) => {
+      let categorySum = 0;
+
+      expensives.forEach((expensive: TransactionData) => {
+        if (expensive.category === category.key) {
+          categorySum += Number(expensive.amount);
+        }
+      });
+
+      if (categorySum > 0) {
+        const total = categorySum.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        });
+
+        totalByCategory.push({
+          key: category.key,
+          name: category.name,
+          total,
+          color: category.color,
+        });
+      }
+    });
+
+    setTotalByCategories(totalByCategory);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <Container>
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
 
-      <HistoryCard color="green" title="Compras" amount="R$ 150,00" />
+      <Content>
+        {totalByCategories.map((item) => (
+          <HistoryCard
+            key={item.key}
+            color={item.color}
+            title={item.name}
+            amount={item.total}
+          />
+        ))}
+      </Content>
     </Container>
   );
 }
